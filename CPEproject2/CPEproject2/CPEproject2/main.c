79da35e2b361a7f0932ff345d149e644400c53bb
antrob25 <55016689+antrob25@users.noreply.github.com>
2020-05-09 15:52:01 -0500

    staring solution

---
+++ CPEproject2/CPEproject2/CPEproject2/main.c
@@ -0,0 +1,18 @@
+/*
+ * CPEproject2.c
+ *
+ * Created: 5/9/2020 3:46:04 PM
+ * Author : Anthony
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
